be0ad7cc236b10672eb1924591797ae11e524919
Arash Izadi <arash.iz@live.com>
2020-12-02 12:02:17 -0800

    Added horizontal movement to player

--- MTEC2210_FP/Assets/Scripts/PlayerScript.cs
+++ MTEC2210_FP/Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float movementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
     }
 }
