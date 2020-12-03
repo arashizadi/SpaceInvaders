f408f38024430affb3a6a3324ee8c61d4b00b84a
Arash Izadi <arash.iz@live.com>
2020-12-03 14:04:32 -0800

    player shooting

--- MTEC2210_FP/Assets/Scripts/BulletScript.cs
+++ MTEC2210_FP/Assets/Scripts/BulletScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public float bulletSpeed;
+    float xFireBullet, yFireBullet;
+    bool shooting;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,28 @@ public class BulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Shoot();
+    }
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && !shooting)
+        {
+            xFireBullet = player.position.x;
+            yFireBullet = player.position.y;
+            transform.position = new Vector2(xFireBullet, yFireBullet);
+            shooting = true;
+        }
+
+        if (shooting)
+        {
+            transform.position += Vector3.up * bulletSpeed * Time.deltaTime;
+            //Deal damage if reaches the enemey
+            //If hits the enemy bullet
+            if (transform.position.y > 3.2f)
+                shooting = false;
+
+        }
+        else
+            transform.position = player.position;
     }
 }
--- MTEC2210_FP/Assets/Scripts/PlayerScript.cs
+++ MTEC2210_FP/Assets/Scripts/PlayerScript.cs
@@ -1,55 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerScript : MonoBehaviour
 {
     public float movementSpeed;
-    public float bulletSpeed;
-    public Transform bullet;
-    bool shooting;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
-        Shoot();
     }
 
     void PlayerMovement()
     {
         if (Input.GetAxis("Horizontal") > 0)
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
-
         else if (Input.GetAxis("Horizontal") < 0)
             transform.position += Vector3.left * movementSpeed * Time.deltaTime;
-    }
-
-    void Shoot()
-    {
-        if (Input.GetButtonDown("Fire1") && !shooting)
-        {
-            Debug.Log("Shoot");
-            for (float i = 0; i < 7.5f; i = i+ 0.10f)
-            {
-                Debug.Log(i);
-                bullet.position += Vector3.up/1000 * bulletSpeed * Time.deltaTime;
-
-            }
-            bullet.position = new Vector2(0, 0);
-            shooting = false;
-            //implement enemy touch or enemy bullet touch
-
-        }
-/*        if(bullet.position.y >= 7.5f)
-        {
-            bullet.position = new Vector2(0, 0);
-            shooting = false;
-        }*/
     }
 }
