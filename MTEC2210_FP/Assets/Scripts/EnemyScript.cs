4a76af1f2b056c7afc883543077cdc8b2d14fb99
Arash Izadi <arash.iz@live.com>
2020-12-03 15:36:14 -0800

    attempting to move enemey

--- MTEC2210_FP/Assets/Scripts/EnemyScript.cs
+++ MTEC2210_FP/Assets/Scripts/EnemyScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Transform Enemies5;
+    public float enemeySpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Enemies5.position += Vector3.right * enemeySpeed * Time.deltaTime;
     }
 }
