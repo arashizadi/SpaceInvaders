111b7595253252cceb5f2d06f8d7616e8885eb93
Arash Izadi <arash.iz@live.com>
2020-12-02 17:50:05 -0800

    attempt to shoot

--- MTEC2210_FP/Assets/Scripts/PlayerScript.cs
+++ MTEC2210_FP/Assets/Scripts/PlayerScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float movementSpeed;
-
+    public float bulletSpeed;
+    public Transform bullet;
+    bool shooting;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +18,38 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        Shoot();
     }
 
     void PlayerMovement()
     {
         if (Input.GetAxis("Horizontal") > 0)
-        {
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
-        }
+
         else if (Input.GetAxis("Horizontal") < 0)
-        {
             transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && !shooting)
+        {
+            Debug.Log("Shoot");
+            for (float i = 0; i < 7.5f; i = i+ 0.10f)
+            {
+                Debug.Log(i);
+                bullet.position += Vector3.up/1000 * bulletSpeed * Time.deltaTime;
+
+            }
+            bullet.position = new Vector2(0, 0);
+            shooting = false;
+            //implement enemy touch or enemy bullet touch
+
         }
+/*        if(bullet.position.y >= 7.5f)
+        {
+            bullet.position = new Vector2(0, 0);
+            shooting = false;
+        }*/
     }
 }
