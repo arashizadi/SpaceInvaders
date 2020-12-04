71f3595d21f885b42a9b6ebef2f73ce4f3750373
Arash Izadi <arash.iz@live.com>
2020-12-03 20:02:53 -0800

    border holds the player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,10 +6,12 @@ using UnityEngine.UIElements;
 public class PlayerScript : MonoBehaviour
 {
     public float movementSpeed;
+    float _movementSpeed;
+    bool playerLock;
     // Start is called before the first frame update
     void Start()
     {
-
+        //movementSpeed = _movementSpeed;
     }
 
     // Update is called once per frame
@@ -24,5 +26,26 @@ public class PlayerScript : MonoBehaviour
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
         else if (Input.GetAxis("Horizontal") < 0)
             transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+
+/*        if (playerLock)
+            movementSpeed = 0;
+        else
+            movementSpeed = _movementSpeed;*/
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.tag == "Wall"))
+        {
+            Debug.Log("sadsa");
+
+            playerLock = true;
+        }
+    }
+
 }
